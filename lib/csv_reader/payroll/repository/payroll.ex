51831c346225ea6b1cdd70c36d4f225c2d17f05b
Jack <le.sastre@ebc.edu.mx>
2021-06-04 00:26:41 -0500

    Adding repository for paysheets db queries

---
+++ lib/csv_reader/payroll/repository/payroll.ex
@@ -0,0 +1,37 @@
+defmodule CsvReader.Repository.Payroll do
+
+  import Ecto.Query, warn: false
+  alias CsvReader.Repo
+
+  alias CsvReader.Model.Paysheet
+
+  def list_all_paysheets() do
+    Repo.all(Paysheet)
+  end
+
+  def get_paysheet!(id) do
+    Paysheet
+    |> Repo.get(id)
+    |> Repo.preload(:batch_block)
+  end
+
+  def get_paysheet_by_params(
+    %{year: year,
+    payment_term: payment_term,
+    division: division,
+    campus: campus,
+    term_code: term_code,
+    enrollment_employee: enrollment_employee,
+    crn: crn
+    }) do
+    Paysheet
+    |> where([p], p.year == ^year)
+    |> where([p], p.payment_term == ^payment_term)
+    |> where([p], p.division == ^division)
+    |> where([p], p.campus == ^campus)
+    |> where([p], p.term_code == ^term_code)
+    |> where([p], p.enrollment_employee == ^enrollment_employee)
+    |> where([p], p.crn == ^crn)
+    |> Repo.all()
+  end
+end
