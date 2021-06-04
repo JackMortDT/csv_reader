512e11ec5c4149c4ec2bc104fa9b741b58e14c28
Jack <le.sastre@ebc.edu.mx>
2021-06-04 14:00:04 -0500

    Remove download sql function from scan view

--- lib/csv_reader_web/controllers/scan_controller.ex
+++ lib/csv_reader_web/controllers/scan_controller.ex
@@ -1,6 +1,8 @@
 defmodule CsvReaderWeb.ScanController do
   use CsvReaderWeb, :controller
 
+  alias CsvReader.Logic.Reader
+
   def index(conn, _params) do
     options = File.ls!("./files/")
     conn
@@ -9,7 +11,13 @@ defmodule CsvReaderWeb.ScanController do
   end
 
   def export_file(conn, params) do
+    Reader.read_file("./files/#{params["file"]}")
+    file = File.read!("./files/updates.sql")
     conn
-    |> render("export.xlsx", %{file: params["file"]})
+    |> put_resp_content_type("text/sql")
+    |> put_resp_header(
+      "content-disposition",
+      "attachment; filename=updates.sql")
+    |> send_resp(200, file)
   end
 end
